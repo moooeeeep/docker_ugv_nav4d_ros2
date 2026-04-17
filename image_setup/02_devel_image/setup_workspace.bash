bdbe639688791e6786294f860a3738e20ea30f1a
Fabian Maas <fama01@posteo.de>
2026-04-17 11:07:03 +0200

    Revert "rename config seed file"
    This reverts commit c39e66036814585d234c3fe18e70a5104a30f432.

--- image_setup/02_devel_image/setup_workspace.bash
+++ image_setup/02_devel_image/setup_workspace.bash
@@ -33,7 +33,7 @@ if [ ! -f ${AUTOPROJ_WS_ROOT}/env.sh ]; then
     git clone $BUILDCONF --branch $BRANCH /tmp/buildconf
     AUTOPROJ_BOOTSTRAP_IGNORE_NONEMPTY_DIR=1 ruby autoproj_bootstrap \
         git $BUILDCONF branch=main \
-        --seed-config=/tmp/buildconf/config_seed.yml \
+        --seed-config=/tmp/buildconf/seed-config.yaml \
         --no-color --no-progress --no-interactive
     rm -rf /tmp/buildconf
 
